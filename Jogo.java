82f3b8d4ca02c1b2ab77fc804173572a45479f4d
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-28 19:41:33 -0300

    Exercicio 1 resolvido

--- Jogo.java
+++ Jogo.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class Jogo {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
         //1. construir um objeto do tipo Personagem
-
+        //variável de referência
+        // Personagem p;
+        // p = new Personagem();
+        Personagem p = new Personagem();
         //2. Configurar um nome para esse objeto
-        
+        System.out.println("Qual o nome do seu personagem?");
+        p.nome = leitor.next();
         //3. Exibir esse nome
+        System.out.println("O nome do seu personagem é: " + p.nome);
     }
 }
