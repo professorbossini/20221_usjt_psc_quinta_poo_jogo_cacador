fc5c660703155036d98e34188a29672f55060696
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-28 20:33:26 -0300

    Caçar implementado

--- Personagem.java
+++ Personagem.java
@@ -2,9 +2,9 @@
 //você deseja que a sua classe seja altamente coesa
 public class Personagem{
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
     
     //metódo tem quatro partes
     //1. tipo de retorno
@@ -12,7 +12,18 @@ public class Personagem{
     //3. lista de parâmetros
     //4. corpo
     void cacar(){
-        System.out.println(nome + " caçando...");
+        if (energia >= 2){
+            System.out.println(nome + " caçando...");
+            //energia = energia - 2;
+            energia -= 2;
+            //energia--;
+            //energia--;
+        }
+        else{
+            System.out.println(nome + " sem energia para cacar");
+        }
+        fome = Math.min (fome + 1, 10);
+        sono = Math.min(sono + 1, 10);
     }
 
     void dormir(){
